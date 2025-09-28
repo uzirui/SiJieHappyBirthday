ea256b26db79a51bf1e93cb511d468dad2eb3ac4
uzirui <2497525862@qq.com>
2025-09-28 17:53:22 +0800

    Update indexl.js

--- js/indexl.js
+++ js/indexl.js
@@ -18,7 +18,7 @@ var S = {
             m++;
       S.Shape.render();
       //console.log(m);
-      if(m==5000){
+      if(m==4500){
         window.location.href="../html/BirthdayCake.html";
       }
     });
